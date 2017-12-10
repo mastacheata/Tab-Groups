e7b977b11707bb4598bb3f444874bf7aaaa2e416
Alex McCausland <alex.mccausland@gmail.com>
2017-12-10 14:53:48 -0500

    Remove unused packagaes, add package-lock

--- rollup.background.config.js
+++ rollup.background.config.js
@@ -2,9 +2,6 @@
 import async from 'rollup-plugin-async'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import uglify from 'rollup-plugin-uglify'
-import livereload from 'rollup-plugin-livereload'
-import serve from 'rollup-plugin-serve'
 // import typescript from 'rollup-plugin-typescript';
 
 let plugins = [
@@ -26,23 +23,6 @@ let config = {
   },
   sourcemap: true,
   plugins: plugins
-}
-
-const isProduction = ( process.env.NODE_ENV === `production` )
-const isDevelopment = ( process.env.NODE_ENV === `development` )
-
-if( isProduction ) {
-  config.sourcemap = false
-  config.plugins.push( uglify() )
-}
-
-if( isDevelopment ) {
-  config.plugins.push( livereload() )
-  config.plugins.push( serve({
-    contentBase: './dist/',
-    port: 8080,
-    open: true
-  }))
 }
 
 export default config
--- rollup.main.config.js
+++ rollup.main.config.js
@@ -1,16 +1,11 @@
 import alias from 'rollup-plugin-alias'
-// import async from 'rollup-plugin-async'
 import vue from 'rollup-plugin-vue'
 import buble from 'rollup-plugin-buble'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeGlobals from 'rollup-plugin-node-globals'
-import uglify from 'rollup-plugin-uglify'
-import livereload from 'rollup-plugin-livereload'
-import serve from 'rollup-plugin-serve'
 
 let plugins = [
-  // async(),
   alias({
     vue$: 'vue/dist/vue.common.js'
   }),
@@ -47,23 +42,6 @@ let config = {
   },
   sourcemap: true,
   plugins: plugins
-}
-
-const isProduction = ( process.env.NODE_ENV === `production` )
-const isDevelopment = ( process.env.NODE_ENV === `development` )
-
-if( isProduction ) {
-  config.sourcemap = false
-  config.plugins.push( uglify() )
-}
-
-if( isDevelopment ) {
-  config.plugins.push( livereload() )
-  config.plugins.push( serve({
-    contentBase: './dist/',
-    port: 8080,
-    open: true
-  }))
 }
 
 export default config
